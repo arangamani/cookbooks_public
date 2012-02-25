e515f5d188b2a315768876d82874afdeb163bea4
Arangamanikkannan Manickam <arangamani.kannan@gmail.com>
2012-02-25 04:29:12 -0500

    ntp completed

--- cookbooks/ntp/recipes/default.rb
+++ cookbooks/ntp/recipes/default.rb
@@ -2,7 +2,20 @@
 # Cookbook Name:: ntp
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, Kannan Rightscale
 #
 # All rights reserved - Do Not Redistribute
 #
+
+package "ntp" do
+	action  [:install]
+end
+
+template "/etc/ntp.conf" do
+	source  "ntp.conf.erb"
+	variables( :ntp_server => "time.nist.gov" )
+end
+
+service "nttpd" do
+	action [:enable,:start]
+end
