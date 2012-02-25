a45554d619220e04be1e219d4b03e027af8cecf2
Arangamanikkannan Manickam <arangamani.kannan@gmail.com>
2012-02-25 04:42:45 -0500

    adding git cookbook from opscode

--- cookbooks/ntp/metadata.rb
+++ cookbooks/ntp/metadata.rb
@@ -4,3 +4,5 @@ license          "All rights reserved"
 description      "Installs/Configures ntp"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+recipe "ntp::default" "Kannan's attempt to install ntp client/server"
