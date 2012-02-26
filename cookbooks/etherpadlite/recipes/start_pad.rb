77ad033711837c06fab58cf47a6a4c8f486feabf
Arangamanikkannan Manickam <arangamani.kannan@gmail.com>
2012-02-26 08:27:30 -0500

    using absolute path for daemonize

--- cookbooks/etherpadlite/recipes/start_pad.rb
+++ cookbooks/etherpadlite/recipes/start_pad.rb
@@ -46,7 +46,7 @@ bash "start_pad" do
   cwd "/var/etherpad-lite/node"
   code <<-EOH
     touch #{node[:etherpadlite][:logfile]}
-    daemonize -o #{node[:etherpadlite][:logfile]} -c /var/etherpad-lite/node -p /var/etherpad-lite/etherpadlited /usr/local/bin/node /var/etherpad-lite/node/server.js
+    /usr/local/sbin/daemonize -o #{node[:etherpadlite][:logfile]} -c /var/etherpad-lite/node -p /var/etherpad-lite/etherpadlited /usr/local/bin/node /var/etherpad-lite/node/server.js
   EOH
 end
 
