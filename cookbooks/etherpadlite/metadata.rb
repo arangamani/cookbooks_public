4f1d6469bc8ba24faab2f443188a5828398d5ddc
Arangamanikkannan Manickam <arangamani.kannan@gmail.com>
2012-02-25 23:45:11 -0500

    adding dependency

--- cookbooks/etherpadlite/metadata.rb
+++ cookbooks/etherpadlite/metadata.rb
@@ -4,6 +4,8 @@ license          "Apache 2.0"
 description      "Installs/Configures etherpadlite"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+depends		 "node"
+
 recipe		 "etherpadlite::prepare_pad", "Installs and prepares etherpad lite"
 recipe		 "etherpadlite::start_pad", "Start the pad"
 
