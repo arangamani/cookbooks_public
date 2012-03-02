921f2d2a0ad66ce5e7eed7d192f9ff3b503f7a16
Arangamanikkannan Manickam <arangamani.kannan@gmail.com>
2012-03-01 22:58:19 -0500

    added recipe in metadata and regenerated json

--- cookbooks/gitosis/metadata.rb
+++ cookbooks/gitosis/metadata.rb
@@ -4,6 +4,7 @@ license          "MIT"
 description      "Installs/Configures gitosis"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1.0"
+recipe 		 "gitosis::default", "Installs/Configures gitosis"
 
 supports "debian"
 
